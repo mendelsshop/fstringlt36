ddc717eeab4e17821d32549017e59d73f103f8a2
mendelsshop <82669516+mendelsshop@users.noreply.github.com>
2021-11-01 09:48:44 -0400

    almost workjg

--- fstrings_lt_37.py
+++ fstrings_lt_37.py
@@ -1,23 +1,30 @@
-def f(string):
-    
-    # string = '{hello} world'
+def f(string):    
     output = ''
     var = ''
     for s in string:
         if s not in ['{','}'] and _coninue != True:
             output += s
-  
         elif s == '{':
             _coninue = True
         elif s == '}':
             _coninue = False
-            var = eval("hello")
-            print(var)
+            exec('global var')
+            # need to work on gloabal and local variables handling
+            var = eval(var)
             output += var
+            var = ''
         elif _coninue == True:
             var += s
+    # handle padding
     return output
-hello = "Hello,"
-string = '{hello} world'
-print(f(string))
+def main():
+    global hello, world
+    hello = "Hello,"
+    world = "world" 
+    string = '{hello} {world}'
+    print(f(string))
+
+if __name__ == '__main__':
+    main()
+
 
