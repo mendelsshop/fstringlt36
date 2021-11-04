fbe6b53fcab1864a05ca0cbb2e24a39b2622676a
mendelsshop <82669516+mendelsshop@users.noreply.github.com>
2021-11-04 15:11:49 -0400

    not working currently

--- fstrings_lt_37.py
+++ fstrings_lt_37.py
@@ -3,49 +3,55 @@ def f(string):
     var = ''
     next = False
     past_semicolon = False
+    past_padding = False
     semicolon_value = ''
     for s in string:
         # make 2 semilcon indicators for semicolon stepping
-        if s not in ['{','}',':'] and next != True and past_semicolon != True:
-            
+        if s not in ['{','}',':'] and next != True and past_semicolon != True and  past_padding != True:
             output += s
         elif s == '{':
             if past_semicolon:
                 next = False
+                continue
             else:
                 next = True
 
         elif s == ':':
             past_semicolon = True
             next = False
-        
+            # handle var so turn var handling into a function
+            
+            continue
+
         elif s == '}':
+            if past_semicolon:
+                past_semicolon = False
+                if semicolon_value:
+                    if semicolon_value[0] in ['<', '>']:
+                        if semicolon_value[0] == '<':
+                            semicolon_value = semicolon_value.strip('< ')
+                            print(semicolon_value)
+                        elif semicolon_value[0] == '>':
+                            semicolon_value = semicolon_value.strip('> ')
+                            print(semicolon_value)
             
-            past_semicolon = False
+            
             next = False
-            # handle semicolon_value maybee use a function
-            semicolon_value = semicolon_value.strip()
-            if semicolon_value:
-                if semicolon_value[0] in ['<', '>']:
-                    if semicolon_value[0] == '<':
-                        semicolon_value = semicolon_value.strip('< ')
-                        semicolon_value = semicolon_value.strip('{}')
-                        print(semicolon_value)
-                    elif semicolon_value[0] == '>':
-                        semicolon_value = semicolon_value.strip('> ')
-                        semicolon_value = semicolon_value.strip('{}')
-                        print(semicolon_value)
-            
+
             exec('global var')
             # need to work on gloabal and local variables handling
+            print(var)
             var = eval(var)
             output += var
             var = ''
             semicolon_value = ''
-        elif past_semicolon == True:
-            semicolon_value += s
         elif next == True:
             var += s
+            
+        if past_semicolon == True:
+            
+            semicolon_value += s
+
 
     # handle padding
     return output
