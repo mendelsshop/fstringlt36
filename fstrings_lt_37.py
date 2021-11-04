bae31daedf0f1dd98ed1ac6b8d3de0d77f09aeb9
mendelsshop <82669516+mendelsshop@users.noreply.github.com>
2021-11-04 07:48:05 -0400

    start passed srmicolon stuffs

--- fstrings_lt_37.py
+++ fstrings_lt_37.py
@@ -1,27 +1,39 @@
 def f(string):    
     output = ''
     var = ''
+    next = False
+    past_semicolon = False
+    semicolon_value = ''
     for s in string:
-        if s not in ['{','}'] and _coninue != True:
+        if s not in ['{','}'] and next != True and past_semicolon != True:
             output += s
         elif s == '{':
-            _coninue = True
+            next = True
+        elif s == ':':
+            past_semicolon = True
+            next = False
         elif s == '}':
-            _coninue = False
+            past_semicolon = False
+            # handle semicolon_value maybee use a function
+            semicolon_value = semicolon_value.strip()
+            print(semicolon_value)
+            next = False
             exec('global var')
             # need to work on gloabal and local variables handling
             var = eval(var)
             output += var
             var = ''
-        elif _coninue == True:
+        elif next == True:
             var += s
+        elif past_semicolon == True:
+            semicolon_value += s
     # handle padding
     return output
 def main():
     global hello, world
     hello = "Hello,"
     world = "world" 
-    string = '{hello} {world}'
+    string = '{hello} {world:  5556  }'
     print(f(string))
 
 if __name__ == '__main__':
