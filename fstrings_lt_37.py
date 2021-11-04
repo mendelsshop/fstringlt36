a039f5cca2ec6fbd781c15319a5c6c5bd062e536
mendelsshop <82669516+mendelsshop@users.noreply.github.com>
2021-11-04 09:49:20 -0400

    stuffs for < and > in fstring parsing

--- fstrings_lt_37.py
+++ fstrings_lt_37.py
@@ -5,35 +5,43 @@ def f(string):
     past_semicolon = False
     semicolon_value = ''
     for s in string:
-        if s not in ['{','}'] and next != True and past_semicolon != True:
+
+        if s not in ['{','}',':'] and next != True and past_semicolon != True:
+            
             output += s
         elif s == '{':
             next = True
         elif s == ':':
             past_semicolon = True
             next = False
+        
         elif s == '}':
             past_semicolon = False
+            next = False
             # handle semicolon_value maybee use a function
             semicolon_value = semicolon_value.strip()
-            print(semicolon_value)
-            next = False
+            if semicolon_value:
+                if semicolon_value[0] in ['<', '>']:
+                    print(semicolon_value[0])
+            
             exec('global var')
             # need to work on gloabal and local variables handling
             var = eval(var)
             output += var
             var = ''
-        elif next == True:
-            var += s
+            semicolon_value = ''
         elif past_semicolon == True:
             semicolon_value += s
+        elif next == True:
+            var += s
+
     # handle padding
     return output
 def main():
     global hello, world
     hello = "Hello,"
     world = "world" 
-    string = '{hello} {world:  5556  }'
+    string = '{hello} {world: < 5556  }'
     print(f(string))
 
 if __name__ == '__main__':
