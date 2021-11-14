011f4da3cbc7da1eabe4e5fdc4ee78571c1a8d42
Mendel <mendelsshop@gmail.com>
2021-11-14 01:21:33 -0500

    consolidated files together

--- fstrings_lt_37.py
+++ fstrings_lt_37.py
@@ -1,104 +1,106 @@
-import f_string_class_idea as fs
 #combine var_handle and f to make a function that can handle all the var stuff
-def var_handle(var): # -> str
-    try:
-        var = eval(var)
+# fix spagghetti code in f class 
 
-    except SyntaxError:
-        pass
+
+
+class f(str):
+    def __init__(self, string) -> None:
+        self.string = string
+    def var_handle(self,var) -> str:
+        # need to work on gloabal and local variables handling
+        
+        try:
+            exec('global eval(var)')
+            var = eval(var)
+
+        except SyntaxError:
+            pass
  
-    return var
+        return var
+    def change_to_fstring(self) -> str:
+        output = ''
+        var = ''
+        next = False
+        past_semicolon = False
+        past_padding = False
+        semicolon_value = ''
+        for s in self.string:
+            # make 2 semilcon indicators for semicolon stepping
+            # print(len(output))
+            # print(output+'t')
+            if s not in ['{','}',':'] and next != True and past_semicolon != True and  past_padding != True:
+                output += s
 
+            elif s == '{':
+                if past_semicolon:
+                    next = False
+                    continue
+                else:
+                    next = True
 
-def f(string): # -> str
-    # need to work on padding size config compared to real fstrings
-
-    output = ''
-    var = ''
-    next = False
-    past_semicolon = False
-    past_padding = False
-    semicolon_value = ''
-    for s in string:
-        # make 2 semilcon indicators for semicolon stepping
-        # print(len(output))
-        # print(output+'t')
-        if s not in ['{','}',':'] and next != True and past_semicolon != True and  past_padding != True:
-            output += s
-
-        elif s == '{':
-            if past_semicolon:
+            elif s == ':':
+                past_semicolon = True
                 next = False
+                # handle var so turn var handling into a function
+                var = self.var_handle(var)
+            
                 continue
-            else:
-                next = True
 
-        elif s == ':':
-            past_semicolon = True
-            next = False
-            # handle var so turn var handling into a function
-            var = var_handle(var)
+            elif s == '}':
+                next = False
+
+                if past_semicolon == True:
+                    past_semicolon = False
+
+                    if semicolon_value:
+
+                        if semicolon_value[0] in ['<', '>']:
+
+                            if semicolon_value[0] == '<':
+                                semicolon_value = semicolon_value.strip('< ')
+                                space = ' ' * int(semicolon_value)
+
+                                var = var + space
+
+                            elif semicolon_value[0] == '>':
+                                semicolon_value = semicolon_value.strip('> ')
+                            
+                                space = ' ' * int(semicolon_value)
+                            
+                                var =  space + var
+
+                elif past_semicolon == False:
+                    var = self.var_handle(var)
+
+                output += var
             
-            continue
+                var = ''
+                semicolon_value = ''
 
-        elif s == '}':
-            next = False
-
+            elif next == True:
+                var += s
+            
             if past_semicolon == True:
-                past_semicolon = False
+                semicolon_value += s
 
-                if semicolon_value:
+        # handle padding
 
-                    if semicolon_value[0] in ['<', '>']:
+        return output
 
-                        if semicolon_value[0] == '<':
-                            semicolon_value = semicolon_value.strip('< ')
-                            space = ' ' * int(semicolon_value)
-                            
-                            var = var + space
+    def __repr__(self) -> str:
+        return self.change_to_fstring()
+    def __str__(self) -> str:
+        return self.change_to_fstring()
 
-                        elif semicolon_value[0] == '>':
-                            semicolon_value = semicolon_value.strip('> ')
-                            
-                            space = ' ' * int(semicolon_value)
-                            
-                            var =  space + var
-            
-            elif past_semicolon == False:
-                var = var_handle(var)
-
-            output += var
-            
-            var = ''
-            semicolon_value = ''
-
-        elif next == True:
-            var += s
-            
-        if past_semicolon == True:
-            semicolon_value += s
-
-    # handle padding
-    # print(output)
-    return output
-
-
-def main(): # -> none
+def main() -> None:
     global hello, world
     hello = "Hello,"
     world = "world" 
     string = '{hello} {world:<8}hiyyy'
     string1 = f'{hello} {world:<13}hiyyy'
-    # block = ''
-    # block = f'{block:<10}'
-    # print(len(block))
-    # print(len(f(string)))
-    # print(len(string1))
-    # print(string1)
-    # 8 = 13
-    # figure out diifreence n padding
 
-    string = '{hello} {world:>8}hiyyy'
+
+    string = f('{hello} {world:>9}hiyyy')
     string1 = f'{hello} {world:>13}hiyyy'
 
     
@@ -106,11 +108,10 @@ def main(): # -> none
     print(f(string))
     print(len(string1))
     print(string1)
-    print(type(fs.f(string)))
+    # need to fix type
+    print(type(string)) 
+    print(f'{"hello"}')
+
 
 if __name__ == '__main__':
     main()
-
-
-
-
