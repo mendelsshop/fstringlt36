e23832e627621d5a82e03cfd5672af76ddc6ff03
mendelsshop <82669516+mendelsshop@users.noreply.github.com>
2021-11-15 12:36:37 -0500

    to many changes describe in less than 50 chars

--- f-string_lt_37 copy.py
+++ f-string_lt_37 copy.py
@@ -14,7 +14,7 @@ class f(str):
         self.version = '0.0.1-alpha'
     def phase_change(self, phase) -> None:
         self.current_phase = phase
-        print('changing to ' + phase)
+        # print('changing to ' + phase)
         return None
     def var_handle(self,var) -> str:
         # need to work on gloabal and local variables handling
@@ -31,7 +31,7 @@ class f(str):
     def f_string_parse(self) -> str:
         self.current_phase = 'parsing'
         # print('parsing', current_phase)
-        # i = 0
+        self.i = 0
         self.output = ''
         self.var_handling = True
         self.var = ''
@@ -39,97 +39,106 @@ class f(str):
 
 
         def info():
-            print('================================================================')
-            print('| f string parser version: ' + self.version,'|')
-            print('| current phase: ' + self.current_phase)
-            print('| current substring: ' + self.string[i], '|')
-            print('| var: ' + self.var, '|')
-            print('| output: ' + self.output)
-            print('| var handling: ' + str(self.var_handling), '|')
-            print('| dummy_var', self.dummy_var, '|')
-            print('================================================================')
-            print()
+            # print('================================================================')
+            # print('| f string parser version: ' + self.version,'|')
+            # print('| current phase: ' + self.current_phase)
+            # print('| current substring: ' + self.string[self.i], '|')
+            # print('| var: ' + self.var, '|')
+            # print('| output: ' + self.output)
+            # print('| var handling: ' + str(self.var_handling), '|')
+            # print('| dummy_var', self.dummy_var, '|')
+            # print('================================================================')
+            # print()
+            return None
         
 
-        # while i < len(self.string):
-        for i in range(len(self.string)):       
+        while len(self.string) > self.i:
+            print(len(self.output))
+            print(self.output)
+            # for self.i in range(len(self.string)):       
             if self.current_phase == 'parsing':
-                if self.string[i] == '{':
+                if self.string[self.i] == '{':
                     self.phase_change('f_string')
-                    # i += 1
+                    self.i += 1
                     info()
-                    print('changing to f_string parsing')
+                    # print('changing to f_string parsing')
                       
                 else:
-                    self.output += self.string[i]
-                    print(self.output)
-                    # i += 1
+                    self.output += self.string[self.i]
+                    # print(self.output)
+                    self.i += 1
                     info()
-                    print('adding to output')
+                    # print('adding to output')
                    
             elif self.current_phase == 'f_string':
-                if self.string[i] in  ['\"','\''] and self.var_handling == True: 
+                if self.string[self.i] in  ['\"','\''] and self.var_handling == True: 
                     self.var_handling = False
-                    self.var += self.string[i]
-                    # i += 1
+                    self.var += self.string[self.i]
+                    self.i += 1
                     info()
-                    print('adding to var without var handling')
+                    # print('adding to var without var handling')
                    
-                elif self.string[i] in ['\'','\"'] and self.var_handling == False:
+                elif self.string[self.i] in ['\'','\"'] and self.var_handling == False:
                     self.var_handling = True
-                    # i += 1
+                    self.i += 1
                     info()
-                    print('stop adding to var with var handling')
+                    # print('stop adding to var with var handling')
                    
-                elif self.string[i] == '}' and self.var_handling == True:
+                elif self.string[self.i] == '}' and self.var_handling == True:
                     self.phase_change('parsing')
-                    # i += 1
                     if self.var[0] in ['\'','\"']:
                         self.var = self.var.strip('\"\'')
-                        print(self.var)
+                        # print(self.var)
                     else:
                         self.var = self.var_handle(self.var)
-                    # i += 1
-                    self.output += self.var
-                    self.var = ''
-                    info()
-                    print('adding var to output')
+                    if len(self.string) > self.i:
+                        self.i += 1
+                        self.output += self.var
+                        self.var = ''
+                        # print(self.output)
+
+                    else:
+                        self.output += self.var
+                        self.var = ''
+                        self.i += 1
+                    # print('adding var to output')
                     
                    
-                elif self.string[i] == ':' and self.var_handling == True:
+                elif self.string[self.i] == ':' and self.var_handling == True:
                     self.phase_change('after f_string')
-                    # i += 1
-                    
+                    self.i += 1
                     info()
-                    print('changing to after f_string parsing')
+                    # print('changing to after f_string parsing')
                    
                 else:
-                    self.var += self.string[i]
-                    print('adding to var', self.string[i])
-                    # i += 1  
+                    self.var += self.string[self.i]
+                    # print('adding to var', self.string[self.i])
+                    self.i += 1  
                     info()
-                    print('adding to var with var handling')
+                    # print('adding to var with var handling')
 
             elif self.current_phase == 'after f_string':
-                if self.string[i] == '}':
+                if self.string[self.i] == '}':
                     self.phase_change('f_string')
-                    # i += 1
-                    info()
-                    print('changing to f_string parsing')
-                else:
-                    if self.string[i] in ['<','>']:
-                        pass
-                    self.dummy_var += self.string[i]
-                    # i += 1
+                
+
 
                     info()
-                    print('handling padding')
+                    # print('changing to f_string parsing')
+                else:
+                    if self.string[self.i] in ['<','>']:
+                        pass
+                    self.dummy_var += self.string[self.i]
+                    self.i += 1
+
+                    info()
+                    # print('handling padding')
         return self.output
         
 
 
     def __repr__(self) -> str:
-        return self.f_string_parse()
+        return '\'' + self.f_string_parse() + '\''
     def __str__(self) -> str:
         return self.f_string_parse()
 
@@ -141,13 +150,14 @@ def main() -> None:
     # string1 = f'{hello} {world:<13}hiyyy'
 
 
-    string = f('xss{"hello"} {world:>5}')
-    string1 = f'{hello} {world:>5}hiyyy'
+    string = f('{hello} {world}hiyyy')
+    string1 = f'{hello} {world}hiyyy'
 
     
+    
+    print(string)
     print(len(string))
-    print(string)
-    print(string)
+    print('=================')
     print(len(string1))
     print(string1)
     # need to fix type
