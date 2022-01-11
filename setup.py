084cd7eebf945e0eb0ddf2c0c81c0ae3fbf794a0
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2022-01-11 16:18:29 -0500

    update setup.py to regex url and changed version

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setup(
     name='f-string_lt_37',
-    version='0.0.3',
+    version='0.0.1',
     description='Python f-strings for before Python3.7',
     license='MIT',
     long_description=long_description,
     author = 'Mendelsshop',
     author_email = 'mendelsshop@gmail.com',
-    url = 'https://github.com/mendelsshop/f-string_lt_37',
+    url = 'https://github.com/mendelsshop/f-string_lt_37/tree/regex',
     packages=['f-string_lt_37'],
     install_requires=[]
 )
