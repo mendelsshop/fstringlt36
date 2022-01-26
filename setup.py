9aec62c59e60235c3ab462271f5785ea9e2dcc90
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2022-01-26 07:41:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='f-string_lt_37',
+    name='f-string_lt_36',
     version='0.0.3',
     description='Python f-strings for before Python3.7',
     license='MIT',
     long_description=long_description,
     author = 'Mendelsshop',
     author_email = 'mendelsshop@gmail.com',
-    url = 'https://github.com/mendelsshop/f-string_lt_37',
-    packages=['f-string_lt_37'],
+    url = 'https://github.com/mendelsshop/f-string_lt_36',
+    packages=['f-string_lt_36'],
     install_requires=['logging' ,'os', 'sys'],
     packages=find_packages(where='src'),
     classifiers=[
