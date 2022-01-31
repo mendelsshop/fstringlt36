36ef25bae2007f10e927311cf84ad72e20841de3
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2022-01-30 23:47:43 -0500

    check if not () for regex1 for when we have = in a function call

--- src/regexs.py
+++ src/regexs.py
@@ -2,6 +2,7 @@
 import re
 
 regex0 = re.compile(r'\{+.+?\}+', re.MULTILINE | re.UNICODE)
+# i thought i wouldnt to check for 
 regex1 = lambda x : \
-re.compile(r'(?<!\'|\")'+x+'(?!\'|\")(?# regex for a character defined from the lambda not in quotes)'\
+re.compile(r'(?<!\'|\"|\()'+x+r'(?!\'|\"|\))(?# regex for a character defined from the lambda not in quotes)'\
 , re.MULTILINE | re.UNICODE)
