3ac23a990419f3eba36747c52ce52c1b9b3490eb
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2022-01-31 09:04:02 -0500

    updated regec comment

--- src/regexs.py
+++ src/regexs.py
@@ -2,7 +2,6 @@
 import re
 
 regex0 = re.compile(r'\{+.+?\}+', re.MULTILINE | re.UNICODE)
-# i thought i wouldnt to check for 
 regex1 = lambda x : \
-re.compile(r'(?<!\'|\"|\()'+x+r'(?!\'|\"|\))(?# regex for a character defined from the lambda not in quotes)'\
+re.compile(r'(?<!\'|\"|\()'+x+r'(?!\'|\"|\))(?# regex for a character defined from the lambda not in quotes or parentheses)'\
 , re.MULTILINE | re.UNICODE)
