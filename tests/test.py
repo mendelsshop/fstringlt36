aac2d5ec7a301165b67b90e7af674b96ce682434
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2022-02-17 13:07:48 -0500

    moved unitest test to test.py moved tests visual teste ie with print to test_visual.py

--- tests/test.py
+++ tests/test.py
@@ -3,77 +3,36 @@ import os
 import sys
 import unittest
 from unittest import TestCase, main, skipIf, expectedFailure
+
 # i know i should be using unit test
 # but i don't know unit tests so this will do
 if os.name == 'nt':
     sep = '\\'
-
 else:
     sep = '/'
+    
 # gets the path to src directories
 path = os.path.dirname(os.path.realpath(__file__))
 split_path = path.split(sep)
 split_path.pop(-1)
 split_path.append('src')
 path = sep.join(split_path)
-# kinda hacky way to import the fstring module from the src folder
-# because when I tried to import fstring like this:
-# from ..src.f_string_lt_37 import f
-# or any variation of that it would not work
 sys.path.append(path)
 from f_string_lt_36 import f
+
 # creates a test class
 class TestFString(TestCase):
-    # creates a test case
-    def test_f_string(self):
-        s = f("Hello")
-        self.assertEqual(str(s), "Hello")
-
-
-def boxify(list) -> None:
     '''
-    the boxify function is just way to test results in a nice boxify function
-    first we find the larggest string in the list given to the boxify function
-    then we print a hash/number sign times longest string + 4
-    then we go through each iem in the list and print a hash/number sign
-    than using the str.center() method we center the current string by the largest string
-    followed by another hash/number sign
-    than we again do the hash/number sign times longest string + 4
+    this is a test class
     '''
-    # find larggest string in list
-    larggest_string = len(max(list, key=len))
-    print('#' * (larggest_string+4))
-    for item in list:
-        # center each item with .center(larggest_string)
-        print('#', item.center(larggest_string), '#')
-    print('#' * (larggest_string + 4))
-
-
-def main() -> None:
-    '''
-    just for visual testing
-    returns None
-    '''
-
-    hello = "Hello,"
-    worlds = "wo"
-    emoji = "😊"
-    world = {'stuff': 'to'}
-    world['thing'] = 'world'
-    world['list'] = [1, 'hi', 3, 4, 5]
-    world['tuple'] = (1, 2, '4', 5)
-    val = 5 * 5
-    world['set'] = {1, 2, 3, '4', 5}
-    world['function'] = lambda x: x**2
-    string = f('{emoji  !a} {worlds}')
-    string1 = f'{emoji  !a} {worlds}'
-    tests = []
-    tests.append('len of fake f_string ' + str(len(string)))
-    tests.append('fake f_string' + string)
-    tests.append('len of real f_string ' + str(len(string1)))
-    tests.append('real f_string ' + string1)
-    boxify(tests)
-
-
-if __name__ == '__main__':
-    main()
+    # this is a test
+    def test_hello(self):
+        '''
+        this is a test
+        '''
+        # this is a test
+        # create variables
+        hello = "Hello,"
+        worlds = "wo"
+        emoji = "😊"
+        self.assertEqual(f('{hello} {worlds}{emoji}'), 'Hello, wo😊')
