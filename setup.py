518aa6445ccc1da9e039108f4fcd338c426d9318
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2022-02-18 11:50:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='f-string_lt_36-MENDELSSHOP',
+    name='f-string_lt_36',
     version='0.0.2',
     description='Python f-strings for before Python3.6',
     license='MIT',
